f278cb6c5d5333fb365c509442e7f70e1fb08153
Windyl John Reylado <149987321+windyljohn@users.noreply.github.com>
2025-03-17 22:59:29 +0800

    no

--- app/webhooks/stripe/route.jsx
+++ app/webhooks/stripe/route.jsx
@@ -8,7 +8,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req) {
   try {
-    const event = stripe.webhooks.constructEvent(
+    const event = await stripe.webhooks.constructEvent(
       await req.body(),
       req.headers.get("stripe-signature"),
       process.env.STRIPE_WEBHOOK_SECRET
